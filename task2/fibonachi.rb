8da0b9768a429e0bf19d3715501e1552f65cb033
Ruslan Zaytsev <breezzz@gmail.com>
2017-08-14 00:25:01 +0300

    fix fibonachi

--- task2/fibonachi.rb
+++ task2/fibonachi.rb
@@ -1,11 +1,7 @@
 fibonacci_numbers = [0, 1]
-n = 2
-temp = 0
 
-while temp < 100
-  temp= fibonacci_numbers[n-1] + fibonacci_numbers[n-2]
-  fibonacci_numbers[n] = temp if temp < 100
-  n += 1
+while fibonacci_numbers[-1] + fibonacci_numbers[-2] < 100
+  fibonacci_numbers << fibonacci_numbers[-1] + fibonacci_numbers[-2]
 end
 
 puts fibonacci_numbers
